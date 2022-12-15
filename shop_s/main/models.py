11e401511641c0ae10eea5bfb59ec36507d8704b
Vi <vi812x@gmail.com>
2022-12-15 12:24:11 +0300

    Start 1

--- shop_s/main/models.py
+++ shop_s/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100) #Привести к списку
+    email = models.EmailField(max_length=50)
+    password_hash = models.CharField(max_length=20)
+    date_registration = models.DateField(auto_now_add=True)
+    date_activity = models.DateField()
